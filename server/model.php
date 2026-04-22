95cfadb645f93508689a9da7ff6219c23a174845
Elkhia Abd El Hakim <elkhia1@mmi-dev.unilim.fr>
2026-04-22 15:20:36 +0200

    update credentials

--- server/model.php
+++ server/model.php
@@ -14,9 +14,9 @@
  * DBPWD : Mot de passe pour se connecter à la base de données.
  */
 define("HOST", "localhost");
-define("DBNAME", "morap01");
-define("DBLOGIN", "morap01");
-define("DBPWD", "morap01");
+define("DBNAME", "elkhia1");
+define("DBLOGIN", "elkhia1");
+define("DBPWD", "elkhia1");
 
 
 function getAllMovies(){
